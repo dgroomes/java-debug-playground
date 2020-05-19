9c56a5270bfc6d9c73c2f6e3c3e8aa3734480158
David Groomes <dwgroomes@gmail.com>
2020-05-19 00:30:03 -0500

    Stub some example blocking and compute methods

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,12 @@ repositories {
     jcenter()
 }
 
+val slf4jVersion = "1.7.30"
+
 dependencies {
+    implementation(group = "org.slf4j", name = "slf4j-api", version = slf4jVersion)
+    implementation(group = "org.slf4j", name = "slf4j-simple", version = slf4jVersion)
+
     // Use JUnit Jupiter API for testing.
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
 
--- src/main/java/dgroomes/App.java
+++ src/main/java/dgroomes/App.java
@@ -1,11 +1,45 @@
 package dgroomes;
 
+import org.slf4j.Logger;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Random;
+
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(App.class);
+
+    public static final int ITERATIONS = 3;
+    public static final int SLEEP_MILLIS = 1000;
+    public static final Random rand = new Random(0);
+
+    /**
+     * A blocking method that might simulate how a blocking I/O call would behave. It's slow!
+     */
+    public void blockingMethod() throws InterruptedException {
+        Thread.sleep(SLEEP_MILLIS);
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    /**
+     * A method that might simulate how a computational method might behave. It might execute, well, fast!
+     */
+    public void fastComputeMethod() {
+        var sum = rand.nextInt() + rand.nextInt();
+        log.trace("Computed a sum: {}", sum);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        var start = Instant.now();
+        var app = new App();
+        log.debug("Executing {} iterations. Calls to blockingMethod sleep for {} milliseconds", ITERATIONS, SLEEP_MILLIS);
+        for (int i = 0; i < ITERATIONS; i++) {
+            app.fastComputeMethod();
+            app.blockingMethod();
+            app.fastComputeMethod();
+        }
+        var end = Instant.now();
+        var duration = Duration.between(start, end);
+        log.info("Done! Executed in {}", duration);
     }
 }
